f2e6b5e52aeefa175308024a365e52ad6b81d8db
Das <ge75guy@prakt4.regent.e-technik.tu-muenchen.de>
2021-06-29 12:42:33 +0200

    generalizingcode

--- script/combine.py
+++ script/combine.py
@@ -7,61 +7,43 @@ import argparse
 
 def main(file_one, file_two, file_three): #can I make these variable?
 
-    jit_engines = ["tcc", "gcc", "llvm"]
+    loop_iteration={
+        "jit_engines" : ["tcc", "gcc", "llvm"],
+        "mips" : [],
+        "Simulation_Time" : [],
+        "CPU_Time" : [],
+        "CPU_cycle" : []
+    }
 
-    path_one = pathlib.Path(file_one)
-    path_two = pathlib.Path(file_two)
-    path_three = pathlib.Path(file_three)
+    results = {}
+    json_files = [file_one, file_two, file_three]
+    # path_one = pathlib.Path(file_one)
+    # print(path_one)
+    # path_two = pathlib.Path(file_two)
+    # path_three = pathlib.Path(file_three)
 
+    #path = [path_one, path_two, path_three]
 
-    with open(path_one, 'r') as f1, open(path_two, 'r') as f2, open(path_three, 'r') as f3:
+    for index, fname in enumerate(json_files):
 
-
-        dict_one = json.load(f1)
-        dict_two = json.load(f2)
-        dict_three = json.load(f3)
-        dict_one = {k: round(v, 2) for k, v in dict_one.items()}
-        dict_two = {k: round(v, 2) for k, v in dict_two.items()}
-        dict_three = {k: round(v, 2) for k, v in dict_three.items()}
-
-
-        #replacing keys with jit engine attribute
-
-        #dict_one creation
-
-        dict_one[f'mips_{jit_engines[0]}'] = dict_one.pop('mips')
-        dict_one[f'Simulation_Time_{jit_engines[0]}'] = dict_one.pop('Simulation_Time')
-        dict_one[f'CPU_Time_{jit_engines[0]}'] = dict_one.pop('CPU_Time')
-        dict_one[f'CPU_Cycle_{jit_engines[0]}'] = dict_one.pop('CPU_cycle')
-        print(dict_one)
-
-        #dict_two_creation
-
-        dict_two[f'mips_{jit_engines[1]}'] = dict_two.pop('mips')
-        dict_two[f'Simulation_Time_{jit_engines[1]}'] = dict_two.pop('Simulation_Time')
-        dict_two[f'CPU_Time_{jit_engines[1]}'] = dict_two.pop('CPU_Time')
-        dict_two[f'CPU_Cycle_{jit_engines[1]}'] = dict_two.pop('CPU_cycle')
-        print(dict_two)
-
-        #dict_three_creation
-
-        dict_three[f'mips_{jit_engines[2]}'] = dict_three.pop('mips')
-        dict_three[f'Simulation_Time_{jit_engines[2]}'] = dict_three.pop('Simulation_Time')
-        dict_three[f'CPU_Time_{jit_engines[2]}'] = dict_three.pop('CPU_Time')
-        dict_three[f'CPU_Cycle_{jit_engines[2]}'] = dict_three.pop('CPU_cycle')
-        print(dict_three)
+        with open(fname, 'r') as f:
+            json_dict = json.load(f)
+            json_dict = {k: round(v, 2) for k, v in json_dict.items()}
+            loop_iteration["mips"].append(json_dict["mips"])
+            loop_iteration["Simulation_Time"].append(json_dict["Simulation_Time"])
+            loop_iteration["CPU_Time"].append(json_dict["CPU_Time"])
+            loop_iteration["CPU_cycle"].append(json_dict["CPU_cycle"])
+            results[f'mips_{loop_iteration["jit_engines"][index]}'] = loop_iteration["mips"][index]
+            results[f'Simulation_Time_{loop_iteration["jit_engines"][index]}'] = loop_iteration["Simulation_Time"][index]
+            results[f'CPU_Time_{loop_iteration["jit_engines"][index]}'] = loop_iteration["CPU_Time"][index]
+            results[f'CPU_Cycle_{loop_iteration["jit_engines"][index]}'] = loop_iteration["CPU_cycle"][index]
 
 
 
-        final_dict = dict(list(dict_one.items()) + list(dict_two.items()) + list(dict_three.items()))
 
 
-    print(final_dict)
-
-
-
-    with open(path_one, 'w') as f1:
-            json.dump(final_dict, f1)
+    with open(file_one, 'w') as f1:
+            json.dump(results, f1)
 
 
 if __name__ == '__main__':
